e1759a410c8df76da9df4216436b9ddade832b3d
olegkatrichuk <katrichukoleg@gmail.com>
2026-03-26 09:03:44 +0200

    dell pass

--- backend/src/VolunteerRequests/PetZone.VolunteerRequests.Infrastructure/VolunteerRequestsDbContextFactory.cs
+++ backend/src/VolunteerRequests/PetZone.VolunteerRequests.Infrastructure/VolunteerRequestsDbContextFactory.cs
@@ -8,9 +8,11 @@ public class VolunteerRequestsDbContextFactory
 {
     public VolunteerRequestsDbContext CreateDbContext(string[] args)
     {
+        var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__Database")
+            ?? "Host=localhost;Port=5434;Database=petzone_db;Username=petzone;Password=changeme";
+
         var optionsBuilder = new DbContextOptionsBuilder<VolunteerRequestsDbContext>();
-        optionsBuilder.UseNpgsql(
-            "Host=localhost;Port=5434;Database=petzone_db;Username=petzone;Password=Ruslan2802@",
+        optionsBuilder.UseNpgsql(connectionString,
             b => b.MigrationsHistoryTable("__EFMigrationsHistory", "volunteer_requests"));
 
         return new VolunteerRequestsDbContext(optionsBuilder.Options);
